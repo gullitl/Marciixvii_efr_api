d1b80baf475e42286045733e07dc297343585a41
plusembo <plam.l@live.fr>
2020-05-23 11:25:36 -0400

    Using Logger in UtilisateurService and CrudService class

--- Marciixvii.EFR.Api/Services/CrudService.cs
+++ Marciixvii.EFR.Api/Services/CrudService.cs
@@ -1,40 +1,85 @@
 ﻿using Marciixvii.EFR.App.DataAccess.Contexts;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Marciixvii.EFR.App.Services {
     public abstract class CrudService<TEntity> where TEntity : class {
+        protected readonly ILogger<CrudService<TEntity>> _logger;
         protected AppDbContext Context { get; }
 
-        public CrudService(AppDbContext context) {
+        public CrudService(ILogger<CrudService<TEntity>> logger, AppDbContext context) {
+            _logger = logger;
             Context = context;
         }
         public async Task<TEntity> Create(TEntity tEntity) {
-            Context.Set<TEntity>().Add(tEntity);
-            await Context.SaveChangesAsync();
-            return tEntity;
+            try {
+                Context.Set<TEntity>().Add(tEntity);
+                await Context.SaveChangesAsync();
+                return tEntity;
+            } catch(InvalidOperationException ex) {
+                _logger.LogCritical(ex, ex.Message);
+                return null;
+            } catch(DbUpdateException ex) {
+                _logger.LogError(ex, ex.Message);
+                return null;
+            }
+
         }
 
         public async Task<bool> Delete(int id) {
-            TEntity tEntity = await Context.Set<TEntity>().FindAsync(id);
-            if(tEntity == null) {
+            try {
+                TEntity tEntity = await Context.Set<TEntity>().FindAsync(id);
+                if(tEntity == null) {
+                    return false;
+                }
+                Context.Set<TEntity>().Remove(tEntity);
+                await Context.SaveChangesAsync();
+
+                return true;
+            } catch(DbUpdateException ex) {
+                _logger.LogError(ex, ex.Message);
+                return false;
+            } catch(InvalidOperationException ex) {
+                _logger.LogCritical(ex, ex.Message);
                 return false;
             }
-            Context.Set<TEntity>().Remove(tEntity);
-            await Context.SaveChangesAsync();
 
-            return true;
         }
 
-        public async Task<List<TEntity>> GetAll() => await Context.Set<TEntity>().ToListAsync();
+        public async Task<List<TEntity>> GetAll() {
+            try {
+                return await Context.Set<TEntity>().ToListAsync();
+            } catch(InvalidOperationException ex) {
+                _logger.LogCritical(ex, ex.Message);
+                return null;
+            }
+        }
 
-        public async Task<TEntity> GetById(int id) => await Context.Set<TEntity>().FindAsync(id);
+        public async Task<TEntity> GetById(int id) { 
+            try {
+                return await Context.Set<TEntity>().FindAsync(id);
+            } catch(InvalidOperationException ex) {
+                _logger.LogCritical(ex, ex.Message);
+                return null;
+            }
+        }
 
         public async Task<bool> Update(TEntity tEntity) {
-            Context.Entry(tEntity).State = EntityState.Modified;
-            await Context.SaveChangesAsync();
-            return true;
+            try {
+                Context.Entry(tEntity).State = EntityState.Modified;
+                await Context.SaveChangesAsync();
+                return true;
+            } catch(DbUpdateException ex) {
+                _logger.LogError(ex, ex.Message);
+                return false;
+            } catch(InvalidOperationException ex) {
+                _logger.LogCritical(ex, ex.Message);
+                return false;
+            }
+
         }
 
     }
--- Marciixvii.EFR.Api/Services/UtilisateurService.cs
+++ Marciixvii.EFR.Api/Services/UtilisateurService.cs
@@ -4,39 +4,72 @@ using Marciixvii.EFR.App.DataAccess.Contexts;
 using Marciixvii.EFR.App.Models.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Threading.Tasks;
 
 namespace Marciixvii.EFR.App.Services {
     public class UtilisateurService : CrudService<Utilisateur>, IUtilisateurService {
-        public UtilisateurService(AppDbContext context) : base(context) {}
+        public UtilisateurService(ILogger<CrudService<Utilisateur>> logger, AppDbContext context) : base(logger, context) {
+        }
 
-        public async Task<Utilisateur> Login(string username, string password) => await Context.Utilisateurs.
-                                                                                            FirstOrDefaultAsync(u => (u.Username.Equals(username) && u.Password.Equals(password)) ||
-                                                                                                                     (u.Email.Equals(username) && u.Password.Equals(password)));
+        public async Task<Utilisateur> Login(string username, string password) {
+            try {
+                return await Context.Utilisateurs.
+                       FirstOrDefaultAsync(u => (u.Username.Equals(username) && u.Password.Equals(password)) ||
+                                                (u.Email.Equals(username) && u.Password.Equals(password)));
+            } catch(InvalidOperationException ex) {
+                _logger.LogCritical(ex, ex.Message);
+                return null;
+            }
+        }
 
-        public async Task<bool> IsUsernameOrEmailExists(string username, string email) => await Context.Utilisateurs.
-                                                                                            FirstOrDefaultAsync(u => u.Username.Equals(username) ||
-                                                                                                                     u.Password.Equals(email)) != null;
+        public async Task<bool> IsUsernameOrEmailExists(string username, string email) {
+            try {
+                return await Context.Utilisateurs.FirstOrDefaultAsync(u => u.Username.Equals(username) ||
+                                                                      u.Password.Equals(email)) != null;
+            } catch(InvalidOperationException ex) {
+                _logger.LogCritical(ex, ex.Message);
+                return false;
+            }
+        }
 
         public async Task<bool> ChangePassword(int id, string password) {
-            Utilisateur utilisateur = new Utilisateur { Id = id, Password = password };
-            Context.Utilisateurs.Attach(utilisateur);
-            Context.Entry(utilisateur).Property(u => u.Password).IsModified = true;
-            await Context.SaveChangesAsync();
-            return true;
+            try {
+                Utilisateur utilisateur = new Utilisateur { Id = id, Password = password };
+                Context.Utilisateurs.Attach(utilisateur);
+                Context.Entry(utilisateur).Property(u => u.Password).IsModified = true;
+                await Context.SaveChangesAsync();
+                return true;
+            } catch(DbUpdateException ex) {
+                _logger.LogError(ex, ex.Message);
+                return false;
+            } catch(InvalidOperationException ex) {
+                _logger.LogCritical(ex, ex.Message);
+                return false;
+            }
         }
 
         public async Task<bool> ChangeProfile(Utilisateur utilisateur) {
-            Context.Utilisateurs.Attach(utilisateur);
-            EntityEntry<Utilisateur> contextEntry = Context.Entry(utilisateur);
-            contextEntry.Property(u => u.Nom).IsModified = true;
-            contextEntry.Property(u => u.Postnom).IsModified = true;
-            contextEntry.Property(u => u.Prenom).IsModified = true;
-            contextEntry.Property(u => u.Sexe).IsModified = true;
-            contextEntry.Property(u => u.Email).IsModified = true;
-            contextEntry.Property(u => u.Username).IsModified = true;
-            await Context.SaveChangesAsync();
-            return true;
+            try {
+                Context.Utilisateurs.Attach(utilisateur);
+                EntityEntry<Utilisateur> contextEntry = Context.Entry(utilisateur);
+                contextEntry.Property(u => u.Nom).IsModified = true;
+                contextEntry.Property(u => u.Postnom).IsModified = true;
+                contextEntry.Property(u => u.Prenom).IsModified = true;
+                contextEntry.Property(u => u.Sexe).IsModified = true;
+                contextEntry.Property(u => u.Email).IsModified = true;
+                contextEntry.Property(u => u.Username).IsModified = true;
+                await Context.SaveChangesAsync();
+                return true;
+            } catch(DbUpdateException ex) {
+                _logger.LogError(ex, ex.Message);
+                return false;
+            } catch(InvalidOperationException ex) {
+                _logger.LogCritical(ex, ex.Message);
+                return false;
+            }
+            
         }
     }
 }
