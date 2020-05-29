4af048c1c9d71d16bc61d5b43c6f8ab442859ac5
plusembo <plam.l@live.fr>
2020-05-29 19:08:39 -0400

    Implementation of both Decrypt and Encrypt methods

--- Marciixvii.EFR.Api/Helpers/Crypt/AesCryptography.cs
+++ Marciixvii.EFR.Api/Helpers/Crypt/AesCryptography.cs
@@ -1,9 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Marciixvii.EFR.App.Contracts;
+using System;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace Marciixvii.EFR.App.Helpers.Crypt {
-    public class AesCryptography {
+    public class AesCryptography : ICryptography {
+        private readonly AesManaged aes;
+
+        public AesCryptography() {
+            aes  = new AesManaged();
+        }
+        public string Decrypt(byte[] cipher) {
+            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+            using MemoryStream ms = new MemoryStream(cipher);
+            using CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
+            using StreamReader reader = new StreamReader(cs);
+            return reader.ReadToEnd();
+        }
+
+        public byte[] Encrypt(string plain) {
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+            using MemoryStream ms = new MemoryStream();
+            using CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write);
+            using StreamWriter sw = new StreamWriter(cs);
+            sw.Write(plain);
+            return ms.ToArray();
+        }
     }
 }
