50667bdabc6d1673829151e32175c3e7916c17c6
plusembo <plam.l@live.fr>
2020-05-31 18:03:20 -0400

    Remove Id attribute from UtilisateurNewPassword class

--- Marciixvii.EFR.Api/Models/DTOs/UtilisateurNewPassword.cs
+++ Marciixvii.EFR.Api/Models/DTOs/UtilisateurNewPassword.cs
@@ -1,7 +1,6 @@
 ﻿
 namespace Marciixvii.EFR.App.Models.DTOs {
     public class UtilisateurNewPassword {
-        public int Id { get; set; }
         public string Token { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
