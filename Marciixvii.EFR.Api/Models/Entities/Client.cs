17694dda69f8df5d3dab63bcff2404db99e75428
plusembo <plam.l@live.fr>
2020-07-04 02:46:36 -0400

    Update Client pojo

--- Marciixvii.EFR.Api/Models/Entities/Client.cs
+++ Marciixvii.EFR.Api/Models/Entities/Client.cs
@@ -5,21 +5,27 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Marciixvii.EFR.App.Models.Entities {
     [Table("client")]
     public class Client: EntityBase {
-        public string NomComplet { get; set; }
+        public string Nom { get; set; }
+        public string Postnom { get; set; }
+        public string Prenom { get; set; }
         public int Telephone { get; set; }
         public string Adresse { get; set; }
         public Sexe Sexe { get; set; }
 
         public override bool Equals(object obj) {
             return obj is Client client &&
-                   NomComplet == client.NomComplet &&
+                   Nom == client.Nom &&
+                   Postnom == client.Postnom &&
+                   Prenom == client.Prenom &&
                    Telephone == client.Telephone &&
                    Sexe == client.Sexe;
         }
 
         public override int GetHashCode() {
             HashCode hash = new HashCode();
-            hash.Add(NomComplet);
+            hash.Add(Nom);
+            hash.Add(Postnom);
+            hash.Add(Prenom);
             hash.Add(Telephone);
             hash.Add(Sexe);
             return hash.ToHashCode();
